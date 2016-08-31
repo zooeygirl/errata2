d0f543b4a5bd0331501f1d60caccefc312ca946e
Brooke Stephenson <zooeythecalisi@gmail.com>
2016-08-31 15:52:20 +0200

    teacher comments

--- app/models/mistake.rb
+++ app/models/mistake.rb
@@ -6,7 +6,7 @@ has_many :essays, :through => :sentences
 has_many :links
 
 
-enum category: {'Vocabulary': 0, 'Verb': 1, 'Agreement': 2, 
+enum category: {'Vocabulary': 0, 'Verb': 1, 'Adjectives + Adverbs': 2, 
 'Linking Words': 3, 'Word Order': 4, 'Pronoun': 5,
 'Sentence Structure': 6, 'Noun': 7, 'Determiner': 8, 'Collocation': 9}
 

13005c61d5b1480524b2f1987d61692527acaab2
Brooke Stephenson <zooeythecalisi@gmail.com>
2016-11-01 10:52:27 +0100

    added sentence pattern

--- app/models/mistake.rb
+++ app/models/mistake.rb
@@ -8,6 +8,6 @@ has_many :links
 
 enum category: {'Vocabulary': 0, 'Verb': 1, 'Adjectives + Adverbs': 2, 
 'Linking Words': 3, 'Word Order': 4, 'Pronoun': 5,
-'Sentence Structure': 6, 'Noun': 7, 'Determiner': 8, 'Collocation': 9, 'Punctuation': 10, 'Register': 11, 'Meaning': 12, 'Negation': 13, 'Function': 14}
+'Sentence Structure': 6, 'Noun': 7, 'Determiner': 8, 'Collocation': 9, 'Punctuation': 10, 'Register': 11, 'Meaning': 12, 'Negation': 13, 'Function': 14, 'Sentence Pattern': 15}
 
 end

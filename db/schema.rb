4b6e21cfbe600f6bf8c5040996d9f1763cae6793
zooeygirl <zooeythecalisi@gmail.com>
2017-06-13 09:14:42 +0200

    change sign-in

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170504062208) do
+ActiveRecord::Schema.define(version: 20170610205517) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,7 +80,7 @@ ActiveRecord::Schema.define(version: 20170504062208) do
     t.string   "name"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
-    t.string   "hundred"
+    t.integer  "hundred"
   end
 
   create_table "grade_elements", force: :cascade do |t|

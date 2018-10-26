2731b7fec0c5fe57c2121551d37ea0f60e86c24b
zooeygirl <zooeythecalisi@gmail.com>
2018-10-26 23:22:42 +0200

    env var

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,12 +99,6 @@ Rails.application.configure do
   password: ENV["GMAIL_PASSWORD"]
   }
 
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-  if ENV["RAILS_LOG_TO_STDOUT"].present?
-    logger           = ActiveSupport::Logger.new(STDOUT)
-    logger.formatter = config.log_formatter
-    config.logger = ActiveSupport::TaggedLogging.new(logger)
-  end
 
 
 end

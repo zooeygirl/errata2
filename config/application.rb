cbce4cdd70404e46225585164870eeba52df7022
zooeygirl <zooeythecalisi@gmail.com>
2018-10-27 11:52:02 +0200

    a

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,6 @@ module Errata
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.serve_static_assets = true
+    config.serve_static_files = true
   end
 end

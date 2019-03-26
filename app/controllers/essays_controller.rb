da1ca0a5b186f2107a4af4abe31daf6ddf67d5a6
zooeygirl <zooeythecalisi@gmail.com>
2019-03-26 22:08:11 +0100

    march 26 9

--- app/controllers/essays_controller.rb
+++ app/controllers/essays_controller.rb
@@ -147,14 +147,6 @@ end
   end
 
 
-def matchMistakes(essay)
-  emis = []
-  essay.sentences.each do |s|
-    emis = emis.concat(s.mistake_ids)
-  end
-  essay.update_attribute(:mistake_ids, emis)
-  essay.save
-end
 
 
   # PATCH/PUT /essays/1
@@ -171,12 +163,7 @@ end
         end
         
         calculate_student_grade
-        emis = []
-        @essay.sentences.each do |s|
-          emis = emis.concat(s.mistake_ids)
-        end
-        @essay.update_attribute(:mistake_ids, emis)
-        @essay.save
+       
         
         format.html { redirect_to session.delete(:return_to), notice: 'Essay was successfully updated.' }
         format.json { render :show, status: :ok, location: @essay }
